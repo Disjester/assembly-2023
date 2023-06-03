d654cf35e1b12ad169818e0be4302b0c52956f55
solek7 <soleiker787@gmail.com>
2023-06-03 14:49:32 +0300

    Let's see the result

---
+++ headers/changes.c
@@ -0,0 +1,25 @@
+/**
+ * @file changes.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-06-03
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <stdio.h>
+
+/**
+ * @brief 
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char const *argv[])
+{
+    printf("something\n");
+    return 0;
+}
