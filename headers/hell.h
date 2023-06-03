3c2dc335de23b6e7c4c3f356b6d67f651c8e915f
solek7 <soleiker787@gmail.com>
2023-06-03 14:24:17 +0300

    testingHereAndThere

--- headers/hell.h
+++ headers/hell.h
@@ -3,5 +3,18 @@
 int main () {
     printf("OMG WTF IS THIS ? ");
     printf("TEST");
+    printf("doing somethingggggggggggggggggggg");
+
+    printf("doing somethingggggggggggggggggggg");
+    printf("doing somethingggggggggggggggggggg");
+
+
+    printf("doing somethingggggggggggggggggggg");
+
+
+    printf("doing somethingggggggggggggggggggg");
+
+    printf("doing somethingggggggggggggggggggg");
+    printf("doing somethingggggggggggggggggggg");
 }
 
