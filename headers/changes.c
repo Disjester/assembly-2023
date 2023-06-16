99346e3a56b66aed52a9a9c466f08a8839ea8286
Boris <boryafeiman@gmail.com>
2023-06-16 18:54:10 +0300

    new

--- headers/changes.c
+++ headers/changes.c
@@ -1,14 +1,3 @@
-/**
- * @file changes.c
- * @author your name (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2023-06-03
- * 
- * @copyright Copyright (c) 2023
- * 
- */
-
 #include <stdio.h>
 
 /**
