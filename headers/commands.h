fba0aa25bfa4fdb960a09c0b11a9dbc4bf75e546
solek7 <soleiker787@gmail.com>
2023-06-25 19:49:20 +0300

    esrsfgawfg

--- headers/commands.h
+++ headers/commands.h
@@ -1,6 +1,8 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
-#include "Assembly-2023\libs.h"
+
+#define MAX_COMMANDS 10
+#define MAX_COMMAND_LENGTH 20
 typedef struct Command{
     char command[MAX_COMMAND_LENGTH];
     char binaryValue[MAX_COMMAND_LENGTH];
