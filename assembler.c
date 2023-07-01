a5e2dc027effa70e3d0846baf0a99f5414234d53
solek7 <soleiker787@gmail.com>
2023-07-01 14:52:59 +0300

    isLabel

--- assembler.c
+++ assembler.c
@@ -1,5 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "libs.h"
 
 void firstIteration(short* memory, CodeNode* code_node) {
+    
+}
+
+bool isLabel(char* word){
+    bool flag = false;
+    int i = 0;
+    if (!isalpha(word[i]))
+    {
+        return flag;
+    }
+    
+    for (; word[i] != '\0'; i++)
+    {
+        if (!isalpha(word[i]) && !isdigit(word[i]))
+        {
+            return flag;
+        }
+        
+    }
+    flag = true;
+    return flag;
     
 }
--- headers/constants.h
+++ headers/constants.h
@@ -4,7 +4,10 @@
 #define MAX_LINE_LENGTH 80
 #define MAX_TOKENS 10
 
-
+/**
+ * @brief a boolean type , that can hold either true or false;
+ * 
+ */
 typedef enum bool{
     false = 0,
     true = 1
