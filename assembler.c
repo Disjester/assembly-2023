29b2c03390fd000bacaad434ece5927f1fc20702
Boris <boryafeiman@gmail.com>
2023-07-01 15:06:41 +0300

    /

--- assembler.c
+++ assembler.c
@@ -13,7 +13,7 @@ void firstIteration(short* memory, CodeNode* code, LabelNode* labels) {
 
     while(temp_code) {
         if(strstr(temp_code->code_row, ":")) {
-            printf("%d\n", isLabel(temp_code->code_row, strstr(temp_code->code_row, ":")));
+            printf("%s  : %d\n",temp_code->code_row, isLabel(temp_code->code_row, strstr(temp_code->code_row, ":")));
         }
 
 
