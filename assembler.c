67b489f69f19cd37c5e35bd0d1d1a142419593b3
solek7 <soleiker787@gmail.com>
2023-07-11 20:53:51 +0300

    small changes

--- assembler.c
+++ assembler.c
@@ -10,13 +10,12 @@ void firstIteration(short* memory, CodeNode* code, LabelNode* labels, Error* err
     bool label_flag = false;
     int i;
     char** tokens = allocateMemory(MAX_TOKENS * sizeof(char *), error);
-    int DC, IC;
+    int DC, IC = 0;
     int num_tokens = 0;
     int token_idx = 0;
     int memory_idx = 100;
     short data[100];
 
-    DC = IC = 0;
     cleanMemory(memory);
     temp_code = code;
     while(temp_code) {
@@ -166,12 +165,12 @@ bool checkData(char* line, Error* error){
 
 bool isString(char* string){
     int i = 0;
+    bool quote = false;
     if (string[i] != '"')
     {
         return false;
     }
     i++;
-    bool quote = false;
     for ( ; i < strlen(string); i++)
     {
         if (quote)
