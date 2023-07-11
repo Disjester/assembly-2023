f67464e4a4c390831d64d46099715a849b54038e
solek7 <soleiker787@gmail.com>
2023-07-11 21:13:21 +0300

    no doublse

--- assembler.c
+++ assembler.c
@@ -16,12 +16,6 @@ void firstIteration(short* memory, CodeNode* code, LabelNode* labels, Error* err
     int memory_idx = 100;
     short data[100];
 
-    test_label_node = (LabelNode*) allocateMemory(sizeof(LabelNode), error);
-    test_label_node->label_name = "something";
-    test_label_node->label_type = LABEL_TYPE_EXTERNAL;
-    test_label_node->next = NULL;
-    test_label_node->memory_adress = 103;
-    getLabelType("something", test_label_node);
 
     test_label_node = (LabelNode*) allocateMemory(sizeof(LabelNode), error);
     test_label_node->label_name = "something";
