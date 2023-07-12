706abb8e53887f546f54fdc363e043cf5e282feb
Boris <boryafeiman@gmail.com>
2023-07-12 20:31:19 +0300

    .

--- headers/functions.h
+++ headers/functions.h
@@ -17,7 +17,7 @@ bool isLabel(char* word);
 
 void cleanMemory(short* memory);
 
-char* removeSemicolon(char* str);
+char* removeColon(char* str);
 
 void insertNewLabel(LabelNode** label, char* label_name, LabelType label_type, short* memory_idx);
 
