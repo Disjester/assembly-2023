65b385e4bba45f8c561dd81d170217fcfaabe275
Boris <boryafeiman@gmail.com>
2023-08-13 21:22:54 +0300

    .

--- assembler.c
+++ assembler.c
@@ -253,8 +253,8 @@ void firstIteration(short* memory, int* memory_idx, CodeNode* code, LabelNode** 
         } 
         temp_label_node = temp_label_node->next;
     }
-    freeMemory(tokens, NULL, NULL, NULL, NULL, NULL);
-    /*freeMemory(NULL, NULL, NULL, NULL, NULL, *labels);*/
+    /*freeMemory(tokens, NULL, NULL, NULL, NULL, NULL);
+    freeMemory(NULL, NULL, NULL, NULL, NULL, *labels);*/
 
 }
 
